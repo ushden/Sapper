f09ed874ae8792b770d61ff7308908918eb11745
ushden <ushden48@gmail.com>
2020-08-24 15:02:03 +0300

    beta 2.0

--- app.js
+++ app.js
@@ -1,38 +1,49 @@
-'use strict';
-
 function initSapperGame() {
-  const size = 10;
-  const squareSize = 50;
+  'use strict';
+
+  const size = 6;
 
   let bombs = [];
   let numbers = [];
-  const numberColors = ['#f68741', '#fcdf87', '#ef0195', '#1b96f3', '#10133a'];
+  const numberColors = ['#785C07', '#fcdf87', '#ef0195', '#1b96f3', '#10133a'];
   const message = {
     lose: 'Ты пидор!',
     win: 'Ты не пидор! Поздравляю!'
   };
 
-  const bombFrequency = 0.2;
+  const bombFrequency = 0.3;
+  let flags = 0;
+  let flagOnBomb = 0;
 
   let gameOver = false;
 
-  const sapper = document.querySelector('.sapper');
+  // Выводим количество бомб и флагов
+  function showInfo() {
+    const bombCount = document.querySelector('.settings .bomb-count span');
+    const flagCount = document.querySelector('.settings .flag-count span');
 
+    bombCount.innerHTML = bombs.length;
+    flagCount.innerHTML = flags;
+  }
+
+
+  const sapper = document.querySelector('.sapper');
+  const width = sapper.getBoundingClientRect().width;
+
+  // Создание игрового поля со всеми даными (Бомбы, цифры, пустые поля)
   function createField() {
 
     for (let i = 0; i < size ** 2; i++) {
       const square = document.createElement('div');
 
       square.classList.add('square');
-      square.style.width = squareSize + 'px';
-      square.style.height = squareSize + 'px';
+      square.style.width = width / size + 'px';
+      square.style.height = width / size + 'px';
 
       sapper.appendChild(square);
     }
 
     const fields = document.querySelectorAll('.square');
-
-    sapper.style.width = size * squareSize + 'px';
 
     let x = 0;
     let y = 0;
@@ -42,7 +53,7 @@ function initSapperGame() {
 
       let randomBoolean = Math.random() < bombFrequency;
 
-      if (randomBoolean) {
+      if (randomBoolean && !numbers.includes(`${x},${y}`)) {
         bombs.push(`${x},${y}`);
 
         if (x > 0) {
@@ -104,61 +115,114 @@ function initSapperGame() {
 
       if (!dataNum) {
         dataNum = 0;
-      } 
+      }
       squareWhisNumber.dataset.number = dataNum + 1;
     });
 
-
+    showInfo();
   }
   createField();
 
+  // Обработка клика по клетке
   function clickOnSquare(target) {
-    if(gameOver) {
+    if (gameOver) {
       return console.log('Game over');
     }
 
     const coords = target.dataset.square;
     const number = target.dataset.number;
 
-    if(bombs.includes(coords) && numbers.includes(coords)) {
+    if (bombs.includes(coords) && !numbers.includes(coords)) {
       target.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
       target.innerHTML = '&#128163';
-      
+
       showPopup(message.lose);
 
       gameOver = true;
     }
-    if(numbers.includes(coords)) {
+
+    if (numbers.includes(coords) && !bombs.includes(coords)) {
       target.innerHTML = number;
       target.style.color = numberColors[number];
       target.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
     }
-    if(!numbers.includes(coords)) {
+
+    if (!numbers.includes(coords) && !bombs.includes(coords) && !number) {
       target.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+      checkSquare(coords);
     }
+
   }
 
+  // Открытие пустых квадратиков, если нету циферок
+  function checkSquare(coordinate) {
+    let coords = coordinate.split(',');
+    let x = parseInt(coords[0]);
+    let y = parseInt(coords[1]);
+
+    setTimeout(() => {
+      if (x > 0) {
+        let targetW = document.querySelector(`[data-square="${x-1},${y}"`);
+        clickOnSquare(targetW);
+      }
+      if (x < size - 1) {
+        let targetE = document.querySelector(`[data-square="${x+1},${y}"`);
+        clickOnSquare(targetE);
+      }
+      if (y > 0) {
+        let targetN = document.querySelector(`[data-square="${x},${y-1}"]`);
+        clickOnSquare(targetN);
+      }
+      if (y < size - 1) {
+        let targetS = document.querySelector(`[data-square="${x},${y+1}"]`);
+        clickOnSquare(targetS);
+      }
+
+      if (x > 0 && y > 0) {
+        let targetNW = document.querySelector(`[data-square="${x-1},${y-1}"`);
+        clickOnSquare(targetNW);
+      }
+      if (x < size - 1 && y < size - 1) {
+        let targetSE = document.querySelector(`[data-square="${x+1},${y+1}"`);
+        clickOnSquare(targetSE);
+      }
+
+      if (y > 0 && x < size - 1) {
+        let targetNE = document.querySelector(`[data-square="${x+1},${y-1}"]`);
+        clickOnSquare(targetNE);
+      }
+      if (x > 0 && y < size - 1) {
+        let targetSW = document.querySelector(`[data-square="${x-1},${y+1}"`);
+        clickOnSquare(targetSW);
+      }
+    }, 10);
+  }
+
+  // Постевить флаг на правую кнопку мыши или долгое нажатие на смартфоне
   function addFlag(target) {
     const coords = target.dataset.square;
 
-    let flags = 0;
+    flags++;
 
-    if(bombs.includes(coords)) {
-      flags++;
+    if (bombs.includes(coords)) {
+      flagOnBomb += 1;
+      console.log(flagOnBomb);
     }
 
     target.innerHTML = '&#128681';
-    console.log(flags);
 
-    isWin(flags);
+    isWin(flagOnBomb);
+    showInfo();
   }
 
+  // Проверка на победу
   function isWin(flags) {
-    if(bombs.length === flags) {
+    if (bombs.length === flags) {
       showPopup(message.win);
     }
   }
 
+  // Показать сообщение о победе или проиграше
   function showPopup(message) {
     const popup = document.createElement('div');
 
@@ -168,23 +232,26 @@ function initSapperGame() {
 
   }
 
-  function restartGame() {
-    const popup = document.querySelector('popup');
+  const newGameBtn = document.querySelector('.settings button');
 
-    gameOver = false;
-    bombs = [];
-    numbers = [];
-    document.body.innerHTML = '';
+  newGameBtn.addEventListener('click', event => {
+    event.preventDefault();
 
-    document.body.removeChild(popup);
+    alert('Пока не работате, обнови страничку, пидр');
+    // restartGame();
+  });
 
-    createField();
-  }
+  // function restartGame() {
+  //   document.body.innerHTML = '';
+
+  //   gameOver = false;
+  //   bombs = [];
+  //   numbers = [];
+
+  //   createField();
+  // }
 
   // tests
-  console.log(bombs);
-  console.log(numbers);
-
 }
 
 initSapperGame();
